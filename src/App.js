25625a805aee163c49037f01186d2da316128ea5
Peter Sidlauskas <peter@bronze56k.com>
2023-03-28 12:07:39 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
       <Row className='text-center align-items-center justify-content-center' style={{position: 'absolute', width: '100%', height: '100vh', padding: '0px 30px', top: '100vh'}}>
         <Col xs={6}>
           <div>
-            <h1 style={{marginBottom: '0px'}}>About Me</h1><h2>I am an Entrepreneur/Video Editor turned Web Developer located in New York City</h2>
+            <h1 style={{marginBottom: '0px'}}>About Me</h1><h3>I am a Film/Video Editor turned Web Developer located in New York City who has been running and operating my own apparel brand for over 10 years</h3>
           </div>
         </Col>
       </Row>
